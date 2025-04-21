cd73dd463ef460f826b3a947fd43da6da9d108be
Vishnu31314 <jangidvishnu258@gmail.com>
2025-04-21 12:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open("C:\\Users\\vishn\\OneDrive\\Desktop\\uber_rides.pkl", "rb"))
+model = pickle.load(open("uber_rides.pkl", "rb"))
 
 @app.route('/')
 def home():
